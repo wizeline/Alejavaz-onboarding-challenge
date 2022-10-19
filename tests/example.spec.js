dcaa93490771b387f0baa6eac01b81269c279905
alejavaz1980 <alejandro.vazquez@wizeline.com>
2022-10-19 11:23:44 -0500

    Initial Commit

---
+++ tests/example.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Given a new user when load the application he is able to access', ()=>{
+
+  test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+
+    
+
+  });
+})
+
+
